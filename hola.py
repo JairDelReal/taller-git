992264b36fcd6d2e71e2edcf3ecf0d2a8db7f62f
DRSJir <jairdelreal@gmail.com>
2025-09-26 15:29:47 -0600

    Agregar comandos magicos

--- hola.py
+++ hola.py
@@ -1,5 +1,7 @@
 def saludar():
     print("Hola, desde la rama")
+    # ssh-keygen -t ed25519 -C "correo@mail.com" -f ~/.ssh/key_taller
+    # cat ~/.ssh/key_taller.pub
 
 if __name__ == "__main__":
     saludar()
